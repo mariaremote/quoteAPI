dba404933cf93c21ee4497a2527f396b7b4066ca
Maria <123465523+mariaremote@users.noreply.github.com>
2024-07-11 21:46:53 +0200

    finishes routing

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
-const { quotes } = require('./data');
-const { getRandomElement } = require('./utils');
-
+const { quotes } = require("./data");
 const PORT = process.env.PORT || 4001;
+const { getRandomElement } = require("./utils");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+app.get("/api/quotes/", (req, res) => {
+  const person = req.query.person;
+  if (person) {
+    const filteredQuotes = quotes.filter((quote) => quote.person === person);
+    res.send({ quotes: filteredQuotes });
+  } else {
+    res.send({ quotes: quotes });
+  }
+});
+
+app.get("/api/quotes/random", (req, res) => {
+  let randomQuote = getRandomElement(quotes);
+  res.send({ quote: randomQuote });
+});
+
+app.post("/api/quotes/", (req, res) => {
+  const newQuote = req.query;
+  if (newQuote.quote && newQuote.person) {
+    quotes.push(newQuote);
+    res.status(201).send({ quote: newQuote });
+  } else {
+    res.status(400).send();
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
